972b73c249fc6a7a1bbc900d7643532163923604
Elielton <elieltonramos14@gmail.com>
2024-05-07 16:31:53 -0300

    feat(back-end): :construction: creating backend folder structure
    Folder structure created based on the MSC model

--- backend-blognet/src/app.ts
+++ backend-blognet/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-// import routes from './routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(routes);
+routes.get('/', (_req, res) => res.json({ message: 'Backend Online' }));
+
+app.use(routes);
 
 export default app;
---
+++ backend-blognet/src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+// import registerRoutes from './registerRoutes'
+
+const routes = Router();
+
+// routes.use('/register', registerRoutes);
+
+export default routes;
