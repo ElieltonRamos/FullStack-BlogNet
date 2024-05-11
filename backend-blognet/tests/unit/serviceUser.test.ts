3c821f4512eebc39e602ebf78151f977e583ad4c
Elielton <elieltonramos14@gmail.com>
2024-05-11 13:39:26 -0300

    refactor(back-end): :white_check_mark: refactor tests

--- backend-blognet/tests/unit/serviceUser.test.ts
+++ backend-blognet/tests/unit/serviceUser.test.ts
@@ -2,11 +2,18 @@ import MemoryDatabase from '../../src/database/memoryDatabase';
 import ServiceRegister from '../../src/services/serviceRegister';
 import User from '../../src/interfaces/user';
 
-describe('tests model user', () => {
+describe('tests service user', () => {
+
+  let memoryModel: MemoryDatabase<User>
+  let registerService: ServiceRegister
+
+  beforeEach(() => {
+    memoryModel = new MemoryDatabase();
+    registerService = new ServiceRegister(memoryModel);
+  });
+
   it('should create a new user', async () => {
-    let memoryModel = new MemoryDatabase<User>();
-    let userService = new ServiceRegister(memoryModel);
-    const response = await userService.create({email: 'test@email.com', password: '123'});
+    const response = await registerService.create({email: 'test@email.com', password: '123'});
     expect(response.status).toBe('created');
     expect(response.data).toHaveProperty('token')
   })
