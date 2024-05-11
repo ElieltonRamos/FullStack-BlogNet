d47526b47c2031be1871d28bde0572d372e566a7
Elielton <elieltonramos14@gmail.com>
2024-05-11 13:44:51 -0300

    test(back-end): :lock: created tests this service register

--- backend-blognet/tests/unit/serviceUser.test.ts
+++ backend-blognet/tests/unit/serviceUser.test.ts
@@ -17,4 +17,12 @@ describe('tests service user', () => {
     expect(response.status).toBe('created');
     expect(response.data).toHaveProperty('token')
   })
+
+  it('should not create a new user with the same email', async () => {
+    const userRegistered = {email: 'created@email.com', password: 'test'}
+    await memoryModel.create(userRegistered);
+    const { status, data } = await registerService.create({email: 'created@email.com', password: 'test'});
+    expect(status).toBe('confict');
+    expect(data).toEqual({ message: 'Email already registered' });
+  });
 })
