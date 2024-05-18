4b92346993da81c15501adfe2597fe2d99f3b58d
Elielton <elieltonramos14@gmail.com>
2024-05-18 10:14:30 -0300

    refactor(controllerLogin): fix catch block variable name
    The catch block in the `ControllerLogin` class was using the variable name `erro` instead of `error`. This commit updates the variable name to `error` for consistency and clarity.

--- backend-blognet/src/controllers/controllerLogin.ts
+++ backend-blognet/src/controllers/controllerLogin.ts
@@ -12,7 +12,8 @@ class ControllerLogin {
       const { email, password } = req.body;
       const { status, data } = await this.serviceLogin.login(email, password);
       return res.status(mapStatusHTTP(status)).send(data);
-    } catch (erro) {
+    } catch (error) {
+      console.log(error);
       return res.status(mapStatusHTTP('serverError')).send({ message: 'Server Error' });
     }
   }
